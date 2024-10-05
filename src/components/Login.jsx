aee3b68aebdc7fcec1107ffec45edbc66a0d45e4
Alejo_PC1 <alejo.prog1426@gmail.com>
2024-10-05 10:09:17 -0500

    Corrigiendo Carga de imagenes

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -48,7 +48,7 @@ const Login = () => {
         <div className="flex flex-col w-full max-w-lg mx-auto p-30 md:p-20 2xl:p-30 bg-[#ffffff] rounded-2xl shadow-xl">
             <div className="flex flex-col justify-center items-center gap-4 pb-4">
                 <div>
-                    <img src="/src/assets/logo.jpeg" width="200" alt="Logo" />
+                    <img src="../../public/assets/logo.jpeg" width="200" alt="Logo" />
                 </div>
                 <h1 className="text-4xl font-bold text-[#006aff]">CASA BLANCA</h1>
             </div>

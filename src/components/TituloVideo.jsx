da3788e336d7a6a853b8e023232552c49fece3a7
Alejo_PC1 <alejo.prog1426@gmail.com>
2024-10-31 18:09:31 -0500

    Pertana torneos

--- src/components/TituloVideo.jsx
+++ src/components/TituloVideo.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import '../css/TituloVideo.css';
 
 const TitleWithVideo = () => {
   const navigate = useNavigate();
@@ -8,7 +9,7 @@ const TitleWithVideo = () => {
 
   useEffect(() => {
     const handleResize = () => {
-      setIsMobile(window.innerWidth < 624);
+      setIsMobile(window.innerWidth < 640);
     };
 
     window.addEventListener('resize', handleResize);
@@ -43,17 +44,17 @@ const TitleWithVideo = () => {
         Tu navegador no soporta la etiqueta de video.
       </video>
       <div className="overlay-layer absolute inset-0 bg-black opacity-50"></div>
-      <div className="content text-center relative flex flex-col h-full justify-center">
-        <h1 className="title-modern text-white text-6xl md:text-5xl font-roboto mt-20">
+      <div className="content text-center relative flex flex-col h-full justify-center p-4">
+        <h1 className="title-modern text-white text-5xl sm:text-6xl md:text-7xl font-roboto mt-20">
           CASA BLANCA<br />
           TENNIS ACADEMY
         </h1>
-        <div className="button-container mt-20 mb-0 flex items-center justify-center">
+        <div className="button-container mt-10 mb-0 flex items-center justify-center">
           <button 
             onClick={handleRegistroClick} 
-            className="cta-button bg-transparent text-2xl bg-opacity-80 backdrop-blur-lg text-white px-6 py-2 rounded-[16px] transition duration-300 hover:bg-orange-500 hover:text-white font-roboto"
+            className="cta-button bg-transparent text-lg sm:text-2xl bg-opacity-80 backdrop-blur-lg text-white px-4 py-2 rounded-[16px] transition duration-300 hover:bg-orange-500 hover:text-white font-roboto"
           >
-            ¡Registrate!
+            ¡Regístrate!
           </button>
         </div>
       </div>
--- src/components/Torneos.jsx
+++ src/components/Torneos.jsx
@@ -1,53 +1,75 @@
+import { useEffect, useState } from 'react';
+import { FaClock, FaCalendarAlt, FaDollarSign, FaBookReader, FaSortAmountDown, FaAward } from 'react-icons/fa';
+import { Link } from 'react-router-dom';
+import Header from './Header';
+import Footer from './Footer';
 import '../css/Torneos.css';
-import { FaClock, FaCalendarAlt, FaDollarSign } from 'react-icons/fa'; // Asegúrate de que esta librería esté instalada
-import { Link } from 'react-router-dom'; // Importa Link
+import { supabase } from '../../supabaseClient';
 
 const Torneos = () => {
-  return (
-    <div className="torneos-page">
-      <h1>Próximos Torneos</h1>
-      <div className="torneos-container">
-        
-        {/* Torneo 1 */}
-        <div className="torneo">
-          <span className="tag">Nivel Avanzado</span>
-          <h2>Torneo de Maestros</h2>
-          <p><FaCalendarAlt /> <strong>Fecha:</strong> 10 de Octubre, 2024</p>
-          <p><FaClock /> <strong>Horario:</strong> 10:00 AM - 2:00 PM</p>
-          <p><FaDollarSign /> <strong>Precio:</strong> $50 por participante</p>
-          <p><strong>Categoría:</strong> Avanzado</p>
-          <Link to="/formulario">
-            <button className="form-button">Formulario</button>
-          </Link>
-        </div>
-        
-        {/* Torneo 2 */}
-        <div className="torneo">
-          <span className="tag">Nivel Intermedio</span>
-          <h2>Torneo de Verano</h2>
-          <p><FaCalendarAlt /> <strong>Fecha:</strong> 15 de Octubre, 2024</p>
-          <p><FaClock /> <strong>Horario:</strong> 1:00 PM - 5:00 PM</p>
-          <p><FaDollarSign /> <strong>Precio:</strong> $30 por participante</p>
-          <p><strong>Categoría:</strong> Intermedio</p>
-          <Link to="/formulario">
-            <button className="form-button">Formulario</button>
-          </Link>
-        </div>
-        
-        {/* Torneo 3 */}
-        <div className="torneo">
-          <span className="tag">Nivel Principiante</span>
-          <h2>Torneo de Iniciación</h2>
-          <p><FaCalendarAlt /> <strong>Fecha:</strong> 20 de Octubre, 2024</p>
-          <p><FaClock /> <strong>Horario:</strong> 9:00 AM - 12:00 PM</p>
-          <p><FaDollarSign /> <strong>Precio:</strong> $20 por participante</p>
-          <p><strong>Categoría:</strong> Principiante</p>
-          <Link to="/formulario">
-            <button className="form-button">Formulario</button>
-          </Link>
-        </div>
+  const [torneos, setTorneos] = useState([]);
+  const [loading, setLoading] = useState(true);
 
+  const fetchTorneos = async () => {
+    const { data, error } = await supabase.from('torneos').select('*');
+
+    if (error) {
+      console.error('Error fetching torneos:', error);
+      return;
+    }
+
+    setTorneos(data);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchTorneos();
+  }, []);
+
+  if (loading) {
+    return <div>Cargando torneos...</div>;
+  }
+
+  return (
+    <div className="flex flex-col min-h-screen relative overflow-hidden">
+      <video 
+        className="absolute top-0 left-0 w-full h-full object-cover z-[-1]"
+        autoPlay
+        loop
+        muted
+      >
+        <source src="/video/Torneos.mp4" type="video/mp4" />
+        Tu navegador no soporta video.
+      </video>
+
+      <Header />
+
+      <div className="torneos-page relative p-6 bg-gray-200 bg-opacity-80 rounded-lg shadow-lg mt-40 flex-grow">
+        <h1 className="text-4xl font-bold text-[#1d3557] font-roboto text-center mb-6">Próximos Torneos</h1>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          {torneos.map(torneo => (
+            <div
+              key={torneo.id}
+              className="torneo p-4 border backdrop-blur-xl rounded-lg shadow-xl hover:shadow-[0_6px_12px_rgba(25,130,196,0.7)] hover:bg-gray-200 transition-shadow duration-300"
+            >
+              <h2 className="text-2xl text-[#1d3557] font-bold text-center mb-4">{torneo.nombre}</h2>
+              <p className="flex items-center"><FaCalendarAlt className="mr-2" /> <strong>Fecha: </strong> {torneo.fecha}</p>
+              <p className="flex items-center"><FaClock className="mr-2" /> <strong>Horario: </strong> {torneo.horario}</p>
+              <p className="flex items-center"><FaDollarSign className="mr-2" /> <strong>Precio: </strong> COP {torneo.precio_torneo}</p>
+              <p className="flex items-center"><FaBookReader className="mr-2" /> <strong>Categoría:</strong> {torneo.categoria}</p>
+              <p className="flex items-center"><FaSortAmountDown className="mr-2" /> <strong>Cupo Máximo: </strong> {torneo.cupo_maximo}</p>
+              <p className="flex items-center"><FaAward className="mr-2" /> <strong>Premios: </strong> {torneo.premios || "No especificado"}</p>
+              <div className="flex justify-center mt-4">
+                <Link to="/formulario">
+                  <button className="form-button bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600 transition duration-300">Formulario</button>
+                </Link>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+
+      <Footer />
     </div>
   );
 };
